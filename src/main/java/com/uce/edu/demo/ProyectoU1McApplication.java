8f51288ea384c87a6dde7800727eae5aa17dc98b
Michael Cen <cenyungamichael@gmail.com>
2022-06-17 20:11:34 -0500

    taller11 Lookup

--- src/main/java/com/uce/edu/demo/ProyectoU1McApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU1McApplication.java
@@ -1,34 +1,38 @@
 package com.uce.edu.demo;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.uce.edu.demo.aeropuerto.modelo.PilotoGeneral;
-import com.uce.edu.demo.aeropuerto.modelo.PilotoPrivado;
-import com.uce.edu.demo.aeropuerto.modelo.RegistroVuelos;
-import com.uce.edu.demo.aeropuerto.modelo.Vuelos;
-import com.uce.edu.demo.aeropuerto.service.IRegistroVuelosService;
+import com.uce.edu.demo.modelo.Estudiante;
+import com.uce.edu.demo.modelo.Materia;
+import com.uce.edu.demo.modelo.Matricula;
+import com.uce.edu.demo.modelo.ProfesorGeneral;
+import com.uce.edu.demo.modelo.ProfesorMateria;
+import com.uce.edu.demo.service.IMatriculaService;
 
 @SpringBootApplication
 public class ProyectoU1McApplication implements CommandLineRunner {
 
 	// inyeccion
-	@Autowired
-	private PilotoGeneral pilotoGeneral1;
-
-	@Autowired
-	private PilotoGeneral pilotoGeneral2;
-
-	@Autowired
-	private PilotoPrivado pilotoPrivado1;
-
-	@Autowired
-	private PilotoPrivado pilotoPrivado2;
-
-	@Autowired
-	private IRegistroVuelosService iRegistroVuelosService;
+		@Autowired
+		private ProfesorGeneral general;
+		
+		
+		@Autowired
+		private ProfesorGeneral general1;
+		
+		@Autowired
+		private ProfesorMateria materia1;
+		
+		@Autowired
+		private ProfesorMateria materia2;
+		
+		@Autowired
+		private IMatriculaService iMatriculaService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU1McApplication.class, args);
@@ -39,31 +43,35 @@ public class ProyectoU1McApplication implements CommandLineRunner {
 		// TODO Auto-generated method stub
 		System.out.println("EJEMPLO SINGLETON");
 
-		this.pilotoGeneral1.setNombre("Michael");
-		this.pilotoGeneral1.setApellido("Cen");
-
-		System.out.println(this.pilotoGeneral1);
+		System.out.println("EJEMPLO SINGLETON");
+		this.general.setNombre("Michael");
+		this.general.setApellido("Cen");
+		
+		System.out.println(this.general);
 		System.out.println("--------------------------------------------");
-		System.out.println(this.pilotoGeneral2);
-		this.pilotoGeneral1.setNombre("Juan");
+		System.out.println(this.general1);
+		this.general1.setNombre("Pepito");
 		System.out.println("--------------------------------------------");
-
-		System.out.println(this.pilotoGeneral1);
+		
+		
+		System.out.println(this.general);
 		System.out.println("------------------------------------------");
-		System.out.println(this.pilotoGeneral2);
-
+		System.out.println(this.general1);
+		
 		System.out.println("EJEMPLO PROTOTYPE");
-		this.pilotoPrivado1.setNombre("Daniel");
-		this.pilotoPrivado1.setApellido("Teran");
-		System.out.println(this.pilotoPrivado1);
-
-		System.out.println(this.pilotoPrivado2);
-
-		RegistroVuelos rVuelo1 = new RegistroVuelos();
-		rVuelo1.setNumVuelo("13215");
-		rVuelo1.setVuelo(new Vuelos());
-
-		this.iRegistroVuelosService.insertar(rVuelo1);
+		this.materia1.setNombre("Daniel");
+		this.materia1.setApellido("Teran");
+		System.out.println(this.materia1);
+		
+		System.out.println(this.materia2);
+		
+		Matricula matricula1=new Matricula();
+		matricula1.setEstudiante(new Estudiante());
+		matricula1.setMateria(new ArrayList<Materia>());
+		matricula1.setNumero("123154");
+		
+		this.iMatriculaService.insertar(matricula1);
+		
 
 	}
 
--- src/main/java/com/uce/edu/demo/service/MatriculaServiceImpl.java
+++ src/main/java/com/uce/edu/demo/service/MatriculaServiceImpl.java
@@ -1,6 +1,7 @@
 package com.uce.edu.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Lookup;
 import org.springframework.stereotype.Service;
 
 import com.uce.edu.demo.modelo.Estudiante;
@@ -20,12 +21,36 @@ public class MatriculaServiceImpl implements IMatriculaService {
 	@Autowired
 	private ProfesorMateria profesorMateria;
 	
+	@Lookup
+	public ProfesorMateria obtenerProfesorM() {
+		//ejercicio de bases de datos de transformacion
+//		ProfesorMateria profe=new ProfesorMateria();
+//		ProfesorMateria profe=this.obtenerProfesorM();
+		
+//		ProfesorMateria profeM=new ProfesorMateria();
+//		profeM.setApellido("Tapia");
+//		profeM.setNombre("Jose");
+		
+		return null;
+	}
+	
+	@Lookup
+	public ProfesorGeneral obtenerProfesorG() {
+		
+		return null;
+	}
+	
+	
 
 	@Override
 	public void insertar(Matricula ma) {
 		// TODO Auto-generated method stub
 		System.out.println("DI desde service SINGLETON "+ this.profesorGeneral);
 		System.out.println("DI desde service PROTOTYPE "+ this.profesorMateria);
+		
+		System.out.println("DI desde un metodo Materia: "+ this.obtenerProfesorM());
+		System.out.println("DI desde un metodo General: "+ this.obtenerProfesorG());
+		
 		this.matriculaRepository.insertar(ma);
 	}
 
